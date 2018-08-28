44f4801cf261e601ff074c5e1b684a42d454fa0a
joseph <imafidonosayanmwen@gmail.com>
2018-08-28 11:42:26 +0100

    fifth

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,14 @@ var attendance = angular.module('attendance',[]);
 
 attendance.controller('attendanceCtrl', ['$http','$q', '$window', function($http,$q,$window) {
     var baseURL = 'http://redoxcorp.eu-4.evennode.com/';
-    
+    var ipName;
         if(ipName.hostname == 'JOSEPH'||ipName.hostname == 'DESKTOP-Q808O43'||ipName.hostname == 'DESKTOP-SB076C5'||ipName.hostname == 'DESKTOP-1QIMCS3'||ipName.hostname == 'DESKTOP-7VB6V3N'||ipName.hostname == 'DESKTOP-25PQQM3') {
-            $window.location.href='/error';
+            console.log("yes");
         }
+    else {
+                    $window.location.href='/error';
+
+    }
     
     var self = this;
     self.code;
